27b78d225a46f03f1bf74dbaa53c206267ec74ac
shyju <shyju.adumkudi@gmail.com>
2019-10-28 14:06:46 +0530

    first commit

---
+++ placeHolder.py
@@ -0,0 +1,10 @@
+# Formatting with place holders
+# You can use %s to inject strings into your print statements. The modulo % is referred to as a "string formatting operator".
+
+print ("I am going to insert %s here" %'something')
+print ('I am going to insert %s %s here' %('something', 'else'))
+
+x, y = 'some', 'more'
+print('I am going to insert %s text here and %s things here' %(x,y))
+
+# Formatting with 
